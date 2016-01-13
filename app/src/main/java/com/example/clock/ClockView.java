acfd500a7987b7f08357130a8d4fe13fc7abb068
jesse <arctic66@sina.com>
2016-01-13 14:20:09 +0800

    更新注释

--- app/src/main/java/com/example/clock/ClockView.java
+++ app/src/main/java/com/example/clock/ClockView.java
@@ -38,76 +38,77 @@ public class ClockView extends View{
 		mHourLen=mMinuteLen*0.88f;
 		mDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss EEEE");
 	}
-	
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		Paint paint = new Paint();
 		paint.setStrokeWidth(mStrokeWidth);
-		paint.setAntiAlias(true);// ���û��ʵľ��Ч��
+		paint.setAntiAlias(true);// 设置画笔的锯齿效果
 		paint.setStyle(Style.STROKE);
 		paint.setColor(Color.WHITE);
-		canvas.translate(canvas.getWidth()/2-mRadius,mRadius); //�ı仭��λ��
-		canvas.drawCircle(mRadius, mRadius, mRadius, paint);//��Ȧ
+		canvas.translate(canvas.getWidth()/2-mRadius,mRadius); //改变画布位置
+		canvas.drawCircle(mRadius, mRadius, mRadius, paint);//外圈
 		Calendar calendar=Calendar.getInstance();
 		Date date=calendar.getTime();
 		int seconds=date.getSeconds();
 		int minutes=date.getMinutes();
 		int hours=date.getHours();
-		
+
 		paint.setColor(Color.WHITE);
 		canvas.drawLine(mRadius, mRadius,
 				mRadius+(float) (mHourLen*Math.sin(((hours+minutes/60f)*30)*Math.PI/180)),
-				mRadius-(float) (mHourLen*Math.cos(((hours+minutes/60f)*30)*Math.PI/180)),paint);//ʱ��ָ��
-		
+				mRadius-(float) (mHourLen*Math.cos(((hours+minutes/60f)*30)*Math.PI/180)),paint);//时针指针
+
 		paint.setColor(Color.BLUE);
 		canvas.drawLine(mRadius, mRadius,
 				mRadius+(float) (mMinuteLen*Math.sin(minutes*6*Math.PI/180)),
-				mRadius-(float) (mMinuteLen*Math.cos(minutes*6*Math.PI/180)),paint);//����ָ��
+				mRadius-(float) (mMinuteLen*Math.cos(minutes*6*Math.PI/180)),paint);//分针指针
 		paint.setColor(Color.RED);
 		canvas.drawLine(
 				mRadius-(float)((mDistance*Math.sin(seconds*6*Math.PI/180))),
 				mRadius+(float)((mDistance*Math.cos(seconds*6*Math.PI/180))),
 				mRadius+(float)((mSecondLen-mDistance)*Math.sin(seconds*6*Math.PI/180)),
-				mRadius-(float)((mSecondLen-mDistance)*Math.cos(seconds*6*Math.PI/180)),paint);//����ָ��
-		
+				mRadius-(float)((mSecondLen-mDistance)*Math.cos(seconds*6*Math.PI/180)),paint);//秒针指针
+
 		paint.setStyle(Style.FILL);
 		canvas.drawCircle(mRadius, mRadius,mSmlRadius, paint);
-		
+
 		paint.setColor(Color.WHITE);
 		paint.setFakeBoldText(mBold);
 		paint.setTextSize(mTextSize);
 		for(int i=1;i<=12;i++){
-			canvas.drawText(i+"", 
-					(float) (mRadius-mTextSize/2+(mRadius-mTextSize*1.5f)*Math.sin(30*i*Math.PI/180)), 
-					(float) (mRadius+mTextSize/2-(mRadius-mTextSize*1.5f)*Math.cos(30*i*Math.PI/180)), paint);//�̶�
+			canvas.drawText(i+"",
+					(float) (mRadius-mTextSize/2+(mRadius-mTextSize*1.5f)*Math.sin(30*i*Math.PI/180)),
+					(float) (mRadius+mTextSize/2-(mRadius-mTextSize*1.5f)*Math.cos(30*i*Math.PI/180)), paint);//刻度
 		}
-		
+
 		paint.setTextSize(mTextSize*2);
 		String str=mDateFormat.format(date);
 		canvas.drawText(str, mRadius-getTextWidth(paint, str)/2, mRadius*2+100, paint);
 	}
-	
+
 	public void updateTime(){
 		invalidate();
 	}
 	/**
-	 * ��ȡ����ĳ���
+	 * 获取字体的长度
 	 * @param paint
 	 * @param str
 	 * @return
 	 */
-	private int getTextWidth(Paint paint, String str) {  
-        int iRet = 0;  
-        if (str != null && str.length() > 0) {  
-            int len = str.length();
-            float[] widths = new float[len];
-            paint.getTextWidths(str, widths);
-            for (int j = 0; j < len; j++) {
-                iRet += (int) Math.ceil(widths[j]);
-            }  
-        }  
-        return iRet;
-    }  
-	
+	private int getTextWidth(Paint paint, String str) {
+		int iRet = 0;
+		if (str != null && str.length() > 0) {
+			int len = str.length();
+			float[] widths = new float[len];
+			paint.getTextWidths(str, widths);
+			for (int j = 0; j < len; j++) {
+				iRet += (int) Math.ceil(widths[j]);
+			}
+		}
+		return iRet;
+	}
+
+
 }
